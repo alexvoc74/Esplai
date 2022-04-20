6d806f0b7ede822a996ef2668266301c0d6c3cea
Alex Voces <alex.voces74@gmail.com>
2022-04-20 12:57:52 +0200

    Ejercicio 3 condicionales

---
+++ JavaScript/Ejercicio003/index.js
@@ -0,0 +1,9 @@
+let luz = (Math.round(Math.random()));
+console.log(luz)
+
+if(luz == 0 ) {
+    console.log('Has abierto la luz y está muy cara ' + '💡' );
+}
+else{
+    console.log('Has cerrado la luz, bien hecho');
+}
