a89af130aa40860d2a60551214fbf73e483db6cc
Alex Voces <alex.voces74@gmail.com>
2022-04-21 11:33:12 +0200

    Array con foreach

---
+++ JavaScript/Ejercicio010/index.js
@@ -0,0 +1,14 @@
+// Array con foreach
+
+const letras = ['a', 'b', 'c', 'd', 'e'];
+
+letras.forEach(function(raul, patito) {
+  console.log('En el indice ' + patito + ' hay este valor: '+  raul);
+  
+});
+
+for(let num=5; num>0; num--){
+  console.log(letras[num-1]);
+}
+
+
