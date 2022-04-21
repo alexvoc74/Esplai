87c0f36c0786e8ad7cb9f9c5a6234cb18b251b24
Alex Voces <alex.voces74@gmail.com>
2022-04-21 11:18:41 +0200

    Trabajando bucle for y while

---
+++ JavaScript/Ejercicio009 bucles/index.js
@@ -0,0 +1,15 @@
+// Bucles For y While
+
+console.log("Tenemos el bucle FOR:");;
+for (let i=0; i<10; i++) {
+  console.log(i+1 + ". I ❤️ code");
+}
+
+console.log("\nY el bucle WHILE:");;
+let num=0;
+while (num < 10) {
+  console.log(num+1 + ". I ❤️ code");
+  num++;
+}
+
+
