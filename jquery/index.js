dc88c936f5d659f93ac1fcb559d6166fc0e8e032
Alex Voces <alex.voces74@gmail.com>
2022-04-22 14:00:33 +0200

    jquery ejercicios

--- jquery/index.js
+++ jquery/index.js
@@ -1,3 +1,17 @@
-$(document).ready(function () {
-    alert('Hola');
-});
+let emojis = ['🥶', '🎪', '👑',' 🗝', '💰' , '🥐', '🌹', '🚀'];
+
+let empuje = [];
+
+//var boton = $('#button01');
+//var display = $('#thingDisplay')
+
+
+
+function iniciar() {
+    empuje.push(emojis[0]);
+    emojis.splice(0,1);
+    console.log(emojis);
+    $('p').remove('.pato');
+    $('#thingDisplay').append(`<p class="pato">${empuje}</p>`);
+    
+}
