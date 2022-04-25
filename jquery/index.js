59e33bca592cdd03cc5a8f3ffbb4b1193cef6a26
Alex Voces <alex.voces74@gmail.com>
2022-04-25 13:38:22 +0200

    Ejercicio gráfico con Arrays

--- jquery/index.js
+++ jquery/index.js
@@ -8,10 +8,58 @@ let empuje = [];
 
 
 function iniciar() {
-    empuje.push(emojis[0]);
-    emojis.splice(0,1);
+    empuje.push(emojis[random()]);
     console.log(emojis);
     $('p').remove('.pato');
-    $('#thingDisplay').append(`<p class="pato">${empuje}</p>`);
-    
+   $('#thingDisplay').append(`<p class="pato">[${empuje}]</p>`);
 }
+
+function drop(){
+    empuje.unshift(emojis[random()]);
+    console.log(emojis);
+    $('p').remove('.pato');
+   $('#thingDisplay').append(`<p class="pato">[${empuje}]</p>`);
+}
+
+function insertAt(){
+
+    var input = document.getElementById('nums'),
+        nums = input.value;
+    empuje.splice(nums, 0, emojis[random()]);
+    $('p').remove('.pato');
+   $('#thingDisplay').append(`<p class="pato">[${empuje}]</p>`);
+}
+function pop() {
+    empuje.pop(emojis[random()]);
+    console.log(emojis);
+    $('p').remove('.pato');
+   $('#thingDisplay').append(`<p class="pato">[${empuje}]</p>`);
+}
+function shift() {
+    empuje.shift(emojis[random()]);
+    console.log(emojis);
+    $('p').remove('.pato');
+   $('#thingDisplay').append(`<p class="pato">[${empuje}]</p>`);
+}
+
+function removeAt() {
+    var input = document.getElementById('numR'),
+        numR = input.value;
+    empuje.splice(numR, 1);
+    $('p').remove('.pato');
+   $('#thingDisplay').append(`<p class="pato">[${empuje}]</p>`);
+}
+
+function random(){
+    numero = Math.random()*7;
+    numero = Math.floor(numero);
+    return  numero;
+}
+
+$(document).ready(function(){
+    $('#button01').click(() => {
+        $('.array')
+            .animate({top: '200px'});
+    });
+});
+
