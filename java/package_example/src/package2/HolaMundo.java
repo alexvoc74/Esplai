c911d4e01364524996a6951ae4ae49b155a8feac
alexvoc74 <alex.voces74@gmail.com>
2022-05-09 13:35:36 +0200

    update

---
+++ java/package_example/src/package2/HolaMundo.java
@@ -0,0 +1,20 @@
+package package2;
+
+public class HolaMundo {
+
+	static String saludo = "Hola Mundo";
+	
+	static String saludar() {
+		
+		System.out.println(saludo);
+		
+		return saludo;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		saludar();
+		
+	}
+
+}
---
+++ java/package_example/src/package2/HolaNunqui.java
@@ -0,0 +1,20 @@
+package package2;
+
+public class HolaNunqui {
+
+	
+	static String saludo = "¡Hasta nunqui!";
+	
+	static String patada() {
+		
+		System.out.println(saludo);
+		
+		return saludo;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		patada();
+	}
+
+}
